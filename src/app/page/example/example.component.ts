d46b1b9ee8f96f2e37d19fbf59fa80fc987d9cb3
Kevin Gonzaga <keviin.gonzaga@gmail.com>
2025-09-01 22:50:38 -0500

    practica con los componentes de angular material

--- src/app/page/example/example.component.ts
+++ src/app/page/example/example.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { InputComponent } from '@components/input/input.component';
 
 @Component({
   selector: 'app-example',
-  imports: [],
+  imports: [InputComponent],
   templateUrl: './example.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExampleComponent { }
+export class ExampleComponent {}
--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './input.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InputComponent { }
+export class InputComponent {}
